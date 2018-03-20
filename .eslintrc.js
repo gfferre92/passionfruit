b7eaf2aba2c9f521ff893605124c815380f909cf
codecolorist <codecolorist@gmail.com>
2018-03-20 20:37:37 +0800

    code style: forbid semicolon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
-    semi: 0,
+    semi: ['error', 'never'],
     'no-console': 0,
     'class-methods-use-this': 0,
     'space-before-function-paren': 0,
