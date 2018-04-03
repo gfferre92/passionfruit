e25f678e2e26ebe38ada6d8f8206e27e298b5bc9
codecolorist <codecolorist@gmail.com>
2018-04-03 20:35:05 +0800

    bugfix: missing classes from bundle frameworks

--- agent/app/classdump.js
+++ agent/app/classdump.js
@@ -1,17 +1,11 @@
 /* eslint camelcase:0, no-cond-assign:0 */
 
-// looks like there's a bug in compiler
-// https://github.com/frida/frida-compile/issues/8
-// eslint-disable-next-line
-null;
-
-function getOwnClasses(sort) {
+function getClassesFromModule(path) {
   const free = new NativeFunction(Module.findExportByName(null, 'free'), 'void', ['pointer'])
   const objc_copyClassNamesForImage = new NativeFunction(
     Module.findExportByName(null, 'objc_copyClassNamesForImage'), 'pointer', ['pointer', 'pointer'])
   const p = Memory.alloc(Process.pointerSize)
   Memory.writeUInt(p, 0)
-  const path = ObjC.classes.NSBundle.mainBundle().executablePath().UTF8String()
   const pPath = Memory.allocUtf8String(path)
   const pClasses = objc_copyClassNamesForImage(pPath, p)
   const count = Memory.readUInt(p)
@@ -21,7 +15,29 @@ function getOwnClasses(sort) {
     classes[i] = Memory.readUtf8String(pClassName)
   }
   free(pClasses)
-  return sort ? classes.sort() : classes
+  return classes
+}
+
+function getOwnModules() {
+  function normalize(path) {
+    return path.replace(/^\/private\/var\//, '/var/')
+  }
+
+  const bundle = normalize(ObjC.classes.NSBundle.mainBundle().bundlePath().toString())
+  return Process.enumerateModulesSync().filter(module =>
+    module.path && normalize(module.path).indexOf(bundle) === 0)
+}
+
+function getOwnClasses(sort) {
+  // todo: filter by module
+  // return a dict
+  let results = []
+  for (const mod of getOwnModules()) {
+    const classes = getClassesFromModule(mod.path)
+    results = results.concat(classes)
+  }
+
+  return sort ? results.sort() : results
 }
 
 function getGlobalClasses(sort) {
@@ -31,6 +47,8 @@ function getGlobalClasses(sort) {
 
 let ownClasses = null
 let globalClasses = null
+
+exports.getOwnModules = getOwnModules
 
 exports.ownClasses = () => {
   if (!ownClasses)
@@ -49,7 +67,7 @@ exports.modules = () => Process.enumerateModulesSync()
 
 exports.exports = name => Module.enumerateExportsSync(name)
 
-exports.inspect = (clazz) => {
+exports.inspect = (clazz, allMethods) => {
   const proto = []
   let clz = ObjC.classes[clazz]
   if (!clz)
@@ -58,8 +76,9 @@ exports.inspect = (clazz) => {
   while (clz = clz.$superClass)
     proto.unshift(clz.$className)
 
+  clz = ObjC.classes[clazz]
   return {
-    methods: ObjC.classes[clazz].$ownMethods,
+    methods: allMethods ? clz.$ownMethods : clz.$methods,
     proto,
   }
 }
