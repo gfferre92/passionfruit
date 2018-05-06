0b8a6212d9a2918fa0bd0bcd51e07e941a6c73d7
codecolorist <codecolorist@gmail.com>
2018-05-06 17:12:01 +0800

    fix: template error when data is empty

--- gui/src/views/tabs/Console.vue
+++ gui/src/views/tabs/Console.vue
@@ -81,7 +81,7 @@ export default {
     }
   },
   filters: {
-    expr: data => data.expr || data,
+    expr: data => (data && data.expr) ? data.expr : data,
     datetime: ts => new Date(ts).toLocaleString('en-US', {
       year: 'numeric', month: '2-digit', day: '2-digit',
       hour: '2-digit', minute: '2-digit', second: '2-digit',
